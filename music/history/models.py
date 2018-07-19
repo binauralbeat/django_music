73d733badfb7c0376085fa12cfb8087fa85b37f1
dan beecroft <binauralbeat001@gmail.com>
2018-07-19 13:29:59 -0500

    just added some models

--- music/history/models.py
+++ music/history/models.py
@@ -6,3 +6,8 @@ class Artist(models>Model):
     artist_name = models.Charfield(max_length=20)
     band_name = models.CharField(max_length=50)
     still_active = models.BooleanField
+
+class Song(models>Model):
+    song_name = models.CharField(max_length=20)
+    song_length = models.CharField(max_length=20)
+    genre = models.CharField(max_length=20)
